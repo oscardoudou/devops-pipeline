8beab14782e34fe807354517929be551f62c61e9
Srija Ganguly <sgangul2@ncsu.edu>
2019-03-10 23:41:50 -0400

    started fuzzer

---
+++ servers/jenkins-server/fuzzer.js
@@ -0,0 +1,13 @@
+var command = require('shelljs');
+var filesystem = require('fs');
+
+// Changes == with !=
+function equalwithnotequalto(file)
+{
+   
+}
+
+// Changes 0 with 1
+
+
+// Reads the file and applies the operations
