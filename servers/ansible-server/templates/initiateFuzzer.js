f5ab328235edbc04b14036dd3b93be5bdaade282
Dyuti <dde@ncsu.edu>
2019-03-12 20:53:25 -0400

    debug fuzzer

--- servers/ansible-server/templates/initiateFuzzer.js
+++ servers/ansible-server/templates/initiateFuzzer.js
@@ -1,1 +1,13 @@
 var shell = require('shelljs');
+
+shell.cd('/home/vagrant/fuzzer/');
+shell.exec('node fuzzer.js');
+
+shell.cd('{{ itrust_folder }}');
+// shell.exec("git add .");
+// shell.exec(`git commit -m "test commit"`);
+
+setTimeout(function(){
+	shell.cd('{{ itrust_folder }}');
+	shell.exec("git revert HEAD");
+}, 10000);
