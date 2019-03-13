95258bd2de83b1e09619a6f5e186c20f59668591
Dyuti <dde@ncsu.edu>
2019-03-13 00:27:22 -0400

    fuzzer commit issue

--- servers/ansible-server/templates/initiateFuzzer.js
+++ servers/ansible-server/templates/initiateFuzzer.js
@@ -4,10 +4,20 @@ shell.cd('/home/vagrant/fuzzer/');
 shell.exec('node fuzzer.js');
 
 shell.cd('{{ itrust_folder }}');
-// shell.exec("git add .");
-// shell.exec(`git commit -m "test commit"`);
+shell.exec
+shell.exec("sudo git add .");
+// setTimeout(function(){
+// 	shell.exec('sudo git commit -m "test commit"');
+// }, 10000);
+if (shell.exec('sudo git commit -am "test commit"').code !== 0) {
+	shell.echo('Error: Git commit failed')
+	shell.exit(1)
+}
+else{
+	shell.echo("hi")
+}
 
-setTimeout(function(){
-	shell.cd('{{ itrust_folder }}');
-	shell.exec("git revert HEAD");
-}, 10000);
+// setTimeout(function(){
+// 	shell.cd('{{ itrust_folder }}');
+// 	shell.exec("git revert HEAD");
+// }, 10000);
