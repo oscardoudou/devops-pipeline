855ac817414ebd06876a5ef52f15746ee11df3ac
Srija Ganguly <sgangul2@ncsu.edu>
2019-03-16 01:20:08 -0400

    analysis update

--- servers/ansible-server/templates/checkboxanalysis.js
+++ servers/ansible-server/templates/checkboxanalysis.js
@@ -3,11 +3,18 @@
 var esprima = require("esprima");
 var options = {tokens:true, tolerant:true, loc:true, range:true};
 var fs = require("fs");
+var shell = require("shelljs");
 
 // alloting threshold above which build should fail
 var maxnumberofconditions = 3;
 var longestmethodlength = 15;
-var securitytoken = false;
+var securitytokenpresent = false;
+
+var failbuild_maxc = false;
+var failbuild_length = false;
+
+var failbuild = false;
+
 
 
 var filepath = ["public_html/codegrams/task/scripts/TaskViewModel.js", 
@@ -39,17 +46,32 @@ var filepath = ["public_html/codegrams/task/scripts/TaskViewModel.js",
 "server-side/site/routes/studyModel.js",
 "server-side/site/routes/upload.js"];
 
+// removes duplicate array elements
+// code credit: "https://codehandbook.org/how-to-remove-duplicates-from-javascript-array/"
+function removeDuplicates(arr){
+    var unique_array = []
+    for(var i = 0;i < arr.length; i++){
+        if(unique_array.indexOf(arr[i]) == -1){
+            unique_array.push(arr[i])
+        }
+    }
+    return unique_array
+}
+
 // Annotates nodes with parent objects.
-function traverseWithParents(object, visitor) {
+function traverseWithParents(object, visitor)
+{
     var key, child;
+
     visitor.call(null, object);
 
     for (key in object) {
         if (object.hasOwnProperty(key)) {
             child = object[key];
-            if (typeof child === 'object' && child !== null && key != 'parent') {
+            if (typeof child === 'object' && child !== null && key != 'parent') 
+            {
             	child.parent = object;
-					    traverseWithParents(child, visitor);
+					traverseWithParents(child, visitor);
             }
         }
     }
@@ -59,136 +81,232 @@ function traverseWithParents(object, visitor) {
 
 
 // Count the max number of conditions within an if statement in a function
-function maxconditions(filepath){
+function maxconditions(filepath)
+{
+
   var max = 0;
   var maxcondition = 0;
   var file = fs.readFileSync(filepath, "utf8");
   var ast = esprima.parse(file, options);
   var line = 0 ;
   var maxline = 0;
-  var tokens = esprima.tokenize(file, options);
+  
 
   // traverse program
 
-  traverseWithParents(ast, function(node) {
-    if(node.type === 'IfStatement') {
+  traverseWithParents(ast, function(node){
+
+   
+
+  	if(node.type === 'IfStatement') {
+
   	  maxcondition = 1;
+
+      
+      
       // traverse its children 
-<<<<<<< HEAD
     
-=======
-      console.log("am in an if statement");
->>>>>>> ca1c754d61a6a65dd8b5ca321f3553e5c21ee643
      	traverseWithParents(node, function(child){
+
+
+
         if(child.type === 'LogicalExpression') {
-          maxcondition = maxcondition + 1;
+
+        	//console.log("am counting");
+
+
+        	maxcondition = maxcondition + 1;
+
+        	//console.log("max: " + maxcondition);
         }
+
+
+
       })
-      if (maxcondition > max) {
-          max = maxcondition;
-      }
-    }
-  })
+     	
+    
+
+     if (maxcondition > max) {
+     	max = maxcondition;
+      
+     }
+      
+
+  	}
+  }
+  	)
   
+  if(max > maxnumberofconditions) // fail build if threshold exceeds
+    failbuild_maxc = true;
+
   return max;
+ 
 }
 
 // returns length of the longest method in a file
 
-function depth(filepath) {
+function depth(filepath)
+{
    var file = fs.readFileSync(filepath, "utf8");
    var ast = esprima.parse(file, options);
    var functionname = "";
    var max = 0;
    var start , stop;
    var maxdepth = 0;
-   var functionmaxdepth = "";
+   var functionmaxdepth;
+
+  // console.log(ast.loc);
 
   // go down the tree to search for function declaration
   traverseWithParents(ast, function(node){
 
     if(node.type === 'FunctionDeclaration') {
+      
+
       functionname = node.id.name;
+
       start = node.loc.start.line;
+
       stop = node.loc.end.line;
+
       maxdepth = stop - start + 1 ;
+
+
     }
 
     if(maxdepth > max){
+     
      max = maxdepth;
      functionmaxdepth = functionname;
-    }
-  })
-  console.log(functionmaxdepth + "() is the longest method");
 
-  return max;
+    }
+
+  })
+
+  if(max > longestmethodlength) // fail the build if threshold exceeds
+    failbuild_length = true;
+  
+
+  return (functionname + " " + max);
+
+
+
 }
 
-// considers a security token to be a word that exceeds 8 characters and that starts with letters and is a combination of letters and digits.
+// considers a security token to be a word that exceeds 10 characters and that starts with letters and is a combination of letters and digits.
 // it could directly flag words such as "password","security", "key" as security tokens too.
-function securitytokens(filepath) {
+function securitytokens(filepath)
+{
 
   var file = fs.readFileSync(filepath, "utf8");
   var ast = esprima.parse(file, options);
+
   var token = "";
   var start = [];
   var startname =[];
   var len = 0;
   var present = false;
   //var regex = /[^[a-zA-Z]+[0-9]+]*|[^[a-zA-z]+[0-9]*]*/;
-  var regex = /[a-zA-Z]+[0-9]*$/ ;
-  var nameregex = [/pass/i, /password/i, /key/i, /security/i];
-
-  // go down the tree for checking a literal 
-  traverseWithParents(ast, function(node){
-    if(node.type == 'Literal' && typeof(node.value) == 'string') {
-      token = node.value;
-      
-      // checking value length and type
-      len = token.length;
-      if(len > 8) {
-        if(regex.test(token)) {
-          start.push(node.loc.start.line);
-          //console.log(token);
-        }
-      }
-    }
-
-    if(node.type == 'VariableDeclarator') {
-        for( var i = 0; i < nameregex.length; i++) {
-          if(nameregex[i].test(node.id.name)) {
-            startname.push(node.loc.start.line);
-          }
-        }
-    }
-  })
+  var regex = /^[a-zA-Z]+[0-9]+$/ ;
   var flag = 1;
 
-  // loop over start array and check if any value is added
+  var nameregex = [/pass/i, /password/i, /key$/i, /security$/i];
+// go down the tree for checking a literal 
 
-  if((start.length >= 1) || (startname.length >= 1)) {
-    console.log("There are security tokens in file : " + filepath);
+ traverseWithParents(ast, function(node){
+  
+   
+   if(node.type == 'Literal' && typeof(node.value) == 'string') {
+    
+     token = node.value;
+     
 
-    for(var j = 0 ; j < start.length; j++) {
-      if(start[j] > 0)
-        console.log("..at line number : " + start[j]);
-    }
-    for(var k = 0; k < startname.length; k++) {
-      for(var l = 0; l < start.length; l++) {
-        if(startname[k] == start[l])
-          flag = 0;
+     // checking value length and type
+
+      len = token.length;
+
+      if(len > 10)
+      {
+         if(regex.test(token))
+         {
+          start.push(node.loc.start.line);
+          //console.log(token);
+
+         }
       }
 
-      if(flag == 1)
-        console.log("..at line number : " + startname[k]);
+   }
 
-    }
-    presence = true;
-  } else {
-    console.log("No security tokens in file : " + filepath);
-  }
+   if(node.type == 'VariableDeclarator')
+   {
+      for( var i = 0; i < nameregex.length; i++)
+      {
+        
+          if(nameregex[i].test(node.id.name))
+         {
 
-  return presence;
+           for(var n = 0 ; n < start.length; n++)
+           {
+            if(start[n] == node.loc.start.line)
+               flag = 0;
+           }
+           if(flag == 1)
+             startname.push(node.loc.start.line);
+
+          
+
+         }
+        
+        
+      }
+   }
+
+
+
+
+ })
+ 
+ var unique;
+ var unique1;
+
+ // loop over start array and check if any value is added
+
+ if((start.length >= 1) || (startname.length >= 1))
+ {
+ // console.log("There may be security tokens in file : " + filepath);
+
+  unique = removeDuplicates(start);
+
+  // for(var j = 0 ; j < unique.length; j++)
+  // {
+  //   if(unique[j] > 0)
+  //    console.log("..at line number : " + unique[j]);
+  // }
+
+  unique1 = removeDuplicates(startname);
+  
+  
+  // for(var k = 0; k < unique1.length; k++)
+  // {
+  //    if(unique1[k] > 0)
+  //     console.log("..at line number : " + unique1[k]);
+
+  // }
+  //presence = true;
+ }
+ else
+ {
+   unique = null;
+   unique1 = null;
+ }
+
+  if(unique != null || unique1 != null) // fail the build if threshold exceeds
+    securitytokenpresent = true;
+
+
+    return (unique + unique1);
+
+  
 }
 
 
@@ -205,26 +323,32 @@ filepath.forEach(function(element){
 
 });
 
-console.log("Maximum conditions within if statments analysis: ");
 
-filepath.forEach(function(element){
-  console.log(maxconditions(element));
+console.log("file           [maxcondition]       [longest method name and length]         [possible securitytokens lines (0=no tokens)]");
+console.log("----------------------------------------------------------------------------------------------------------------------------");
 
+for (var arr =0 ; arr<filepath.length; arr++)
+{
+  console.log(filepath[arr] + "\t[" + maxconditions(filepath[arr]) + "]\t\t[" + depth(filepath[arr]) + "]\t\t[" + securitytokens(filepath[arr]) + "]");
+}
+
+// fail build if the threshold has exceeded
+if(securitytokenpresent || failbuild_maxc || failbuild_length)
+  {
+    failbuild = true;
+    
+  }
+
+fs.writeFile('build.txt', failbuild, function (err) {
+     if (err) throw err;
+     console.log('Saved build requirement in build.txt');
 });
 
-console.log("Long methods analysis: ");
 
-filepath.forEach(function(element){
-  console.log(depth(element));
 
-});
 
-console.log("security tokens analysis: ");
 
-filepath.forEach(function(element){
-  console.log(securitytokens(element));
 
-});
 
 
 
