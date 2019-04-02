eab94d32a501198bd62e48681dd655a618493f35
Arpita Shekhar <ashekha@ncsu.edu>
2019-04-02 18:56:42 -0400

    removed old files

--- servers.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-cd servers/ansible-server && baker bake && cd ../jenkins-server && baker bake && cd ../ansible-server/ && baker ssh
--- servers/ansible-server/templates/groovy_setup_user.groovy
+++
@@ -1,8 +0,0 @@
-#! groovy
-
-import static jenkins.model.Jenkins.instance as jenk
-import jenkins.install.InstallState
-
-if(!jenk.installState.isSetupComplete()) {
-  InstallState.INITIAL_SETUP_COMPLETED.initializeState()
-}
