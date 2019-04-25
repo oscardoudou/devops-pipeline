3566a591039944f4a2afb68a7483ccecc03e6097
yzhan222 <yzhan222@ncsu.edu>
2019-04-25 02:49:08 +0000

    provision dev machine for setting up docker kops awscli kubectl

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - && sudo apt-get install -y nodejs && node -v && sudo npm install npm --global
-mkdir /home/vagrant/jenkins && npm install
+mkdir -p /home/vagrant/jenkins /home/vagrant/dev && npm install
